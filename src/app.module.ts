ed269764ad9f898880b46cac5cd250ec7a28abf7
Olesia_Miller@epam,con <3010Dancer1@>
2024-05-16 14:46:09 +0300

    Task 8 finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
-
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { DBModule } from './db/db.module';
 
 @Module({
   imports: [
     AuthModule,
     CartModule,
     OrderModule,
+    DBModule
   ],
   controllers: [
     AppController,
